f96595f92296cc1560c769ad32922213ee33cb93
Wim Fournier <github@fournier.nl>
2022-09-25 18:10:46 +0200

    move stage out

--- infrastructure/API.tf
+++ infrastructure/API.tf
@@ -1,3 +1,25 @@
+data "external" "git_checkout" {
+  program = ["${path.module}/get_sha.sh"]
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.minecraft.id
+
+  triggers = {
+    redeployment = data.external.git_checkout.result.sha
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "stage" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.minecraft.id
+  stage_name    = "prod"
+}
+
 resource "aws_api_gateway_resource" "server" {
   parent_id   = aws_api_gateway_rest_api.minecraft.root_resource_id
   path_part   = "server"
--- infrastructure/api_resource/api-gateway.tf
+++ infrastructure/api_resource/api-gateway.tf
@@ -23,32 +23,3 @@ resource "aws_lambda_permission" "allow_api_gateway" {
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
   source_arn = "arn:aws:execute-api:${var.region}:${var.account_id}:${var.rest_api_id}/*/${aws_api_gateway_method.method.http_method}/${var.path}"
 }
-
-resource "aws_api_gateway_deployment" "deployment" {
-  rest_api_id = var.rest_api_id
-
-  triggers = {
-    # NOTE: The configuration below will satisfy ordering considerations,
-    #       but not pick up all future REST API changes. More advanced patterns
-    #       are possible, such as using the filesha1() function against the
-    #       Terraform configuration file(s) or removing the .id references to
-    #       calculate a hash against whole resources. Be aware that using whole
-    #       resources will show a difference after the initial implementation.
-    #       It will stabilize to only change when resources change afterwards.
-    redeployment = sha1(jsonencode([
-      var.resource_id,
-      aws_api_gateway_method.method.id,
-      aws_api_gateway_integration.integration.id,
-    ]))
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_api_gateway_stage" "stage" {
-  deployment_id = aws_api_gateway_deployment.deployment.id
-  rest_api_id   = var.rest_api_id
-  stage_name    = "prod"
-}
