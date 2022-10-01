711238531c808bcd211f035d4f33df53ed99bf5d
Wim Fournier <github@fournier.nl>
2022-10-01 14:21:45 +0200

    improved status

--- web/src/components/Server.vue
+++ web/src/components/Server.vue
@@ -11,22 +11,22 @@
             v-on="on"
         >
           {{ server.name }}.${domain_name}
-          &nbsp;
+          <v-spacer></v-spacer>
           <v-progress-circular
-              v-if="0 < statusValue() < 100"
+              v-if="statusValue() > 0 && statusValue() < 100"
               :value="statusValue()"
-              :color="statusValue()===100?'green': statusValue()===0?'red':'orange'"
+              :color="statusValue()===100?'green':statusValue()===0?'red':'orange'"
           />
           <v-icon v-if="statusValue() === 100" color="green">mdi-checkbox-marked-circle-outline</v-icon>
           <v-icon v-if="statusValue() === 0" color="red">mdi-close-circle-outline</v-icon>
         </v-card-title>
         </template>
+        <span>{{ server }}</span>
       </v-tooltip>
       <v-card-text>
         <v-btn v-if="server.last_status === 'NONE'" @click="start_server()">Start</v-btn>
         <v-btn v-if="server.last_status !== 'NONE'" @click="stop_server()">Stop</v-btn>
         <v-alert>{{ error }}</v-alert>
-        <span>{{ server }}</span>
         <v-list>
           <v-list-item v-for="(value,key) in server.tags" v-bind:key="key">
             <v-list-item-title>{{ key }}</v-list-item-title>
